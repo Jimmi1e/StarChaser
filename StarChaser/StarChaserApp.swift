a0a7a02046fc1d000560caecb3faaf4cc86120b2
Jimmi1e <yjxyang2@outlook.com>
2026-06-11 13:34:06 +0800

    Initial Commit

---
+++ StarChaser/StarChaserApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarChaserApp.swift
+//  StarChaser
+//
+//  Created by Jiaxi on 2026/6/11.
+//
+
+import SwiftUI
+
+@main
+struct StarChaserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
